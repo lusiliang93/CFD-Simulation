60eeb0ff89626ba1e74ad4e0e8a1f468510306cb
Jin Hu <thuhujin@gmail.com>
2016-12-14 15:51:09 -0500

    remove print

--- uvp.c
+++ uvp.c
@@ -128,7 +128,7 @@ int poisson(double **p,double **rhs,int imax,int jmax,double delx,double dely,do
         printf("poisson\n");
         for(j=0;j<jmax+2;j++){
         	for(i=0;i<imax+2;i++){
-        		printf("%lf \n", p[j][i]);
+        		printf("%lf ", p[j][i]);
         	}
         	printf(" \n");
         }
